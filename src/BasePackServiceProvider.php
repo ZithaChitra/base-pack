11291b8286512838934b762232f81046286063cf
zithachitra <realblessingchitra@gmail.com>
2023-08-22 13:56:48 +0200

    add middleware

--- src/BasePackServiceProvider.php
+++ src/BasePackServiceProvider.php
@@ -4,7 +4,6 @@ namespace BasePack;
 
 // require_once __DIR__.'/../vendor/autoload.php';
 use Illuminate\Support\ServiceProvider;
-use Blessing\BrandsplashPackage\Http\Middleware\CapitalizeTitle;
 use Illuminate\Contracts\Http\Kernel;
 use Illuminate\Support\Facades\Artisan;
 use Livewire\Livewire;
@@ -12,22 +11,12 @@ use BasePack\Http\Livewire\Auth\Authenticate;
 use BasePack\Http\Livewire\AdminDashboard;
 use BasePack\Http\Livewire\CustomFooter;
 use Illuminate\View\Compilers\BladeCompiler;
+use BasePack\Http\Middleware\IdservAuth;
 
 class BasePackServiceProvider extends ServiceProvider 
 {
     public function register()
     {
-        $this->callAfterResolving(BladeCompiler::class, function(){
-            if(class_exists(Livewire::class)){
-                // Livewire::component('bauth.authenticate', Authenticate::class);
-                // Livewire::component('admin-dashboard', AdminDashboard::class);
-                // Livewire::component('custom-footer', CustomFooter::class);
-            }
-        });
-        // $this->app->bind('calculator', function($app){
-        //     return new Calculator();
-        // });
-        // $this->mergeConfigFrom(__DIR__.'/../config/config.php', 'blogpackage');
     }
 
     public function boot(Kernel $kernel)
@@ -54,8 +43,8 @@ class BasePackServiceProvider extends ServiceProvider
                 __DIR__.'/../resources/public/vendor' => public_path('vendor'),
                 ], 'assets');
         }
-        // // $kernel->pushMiddleware(CapitalizeTitle::class);
-        // $this->loadMigrationsFrom(__DIR__. '/../database/migrations');
+        $kernel->pushMiddleware(IdservAuth::class);
+        $this->loadMigrationsFrom(__DIR__. '/../database/migrations');
         // $this->force
         
         $this->loadRoutesFrom(__DIR__. '/../routes/web.php');
